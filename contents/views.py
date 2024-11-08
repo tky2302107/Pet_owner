a3c1d56d069d573721b1d75133bcf0e94c94fee1
tky230217 <tky2302107@stu.o-hara.ac.jp>
2024-11-08 11:04:41 +0900

    post一時停止、contents_app作成

---
+++ contents/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+# from django.contrib.auth.views import LoginView, LogoutView
+
+class Index(TemplateView):
+    template_name = 'contents/contents.html'
