7c9ee750d18c4a21b243d7d753d19d5a1b33c416
tky230217 <tky2302107@stu.o-hara.ac.jp>
2024-11-08 14:49:54 +0900

    メインページ追記

--- contents/urls.py
+++ contents/urls.py
@@ -5,4 +5,9 @@ app_name = 'contents'
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('/notice', views.Notice.as_view(), name='notice'),
+    path('/points', views.Points.as_view(), name='points'),
+    
+    path('/error404', views.AccessError.as_view(), name='accesserror'),
+    path('/error500', views.ServerError.as_view(), name='servererror'),
 ]
