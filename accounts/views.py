8f0dccd945fe5b6fdf022739659e433708cfd90f
tky230217 <tky2302107@stu.o-hara.ac.jp>
2024-11-12 14:58:09 +0900

    contents更新

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import User
 
@@ -14,13 +14,15 @@ class LogoutPage(LogoutView):
 class MainPage(TemplateView):
     template_name = 'main.html'
     
-class ExchangePoint(TemplateView):
+class ExchangePoint(ListView):
     template_name = 'accounts/p1.html'
     model = User
     def get_queryset(self):
         return super().get_queryset()
-    
+    nl = User.objects.filter(id=1)
+        
     
     
 class ExchangePointComplete(TemplateView):
     template_name = 'accounts/p2.html'
+    
--- contents/views.py
+++ contents/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import NoticeList
 from django.views.generic import ListView
 from .forms import NoticeListForm
-# Create your views here.
 from django.views.generic import TemplateView
 from django.shortcuts import render, get_object_or_404
 # from django.contrib.auth.views import LoginView, LogoutView
@@ -16,33 +15,37 @@ class NoticeList(ListView):
     model = NoticeList
     
     
-    # nl = NoticeList.objects.all().order_by('-posted_at')#models.pyより、通知を全権取得＆日付順に整列
+    
         
-    def get_queryset(self, **kwargs):
-        queryset = super().get_queryset(**kwargs) # Article.objects.all() と同じ結果
+    # def get_queryset(self, **kwargs):
+    #     queryset = super().get_queryset(**kwargs) # Article.objects.all() と同じ結果
 
-        # is_publishedがTrueのものに絞り、titleをキーに並び変える
-        queryset = queryset.filter().order_by('-id')
+    #     # is_publishedがTrueのものに絞り、titleをキーに並び変える
+    #     queryset = queryset.filter().order_by('-id')
 
-        return queryset
+    #     return queryset
+    def get_queryset(self):
+        return super().get_queryset()
     
+    nl = NoticeList.objects.all().order_by('-id')#models.pyより、通知を全権取得＆日付順に整列
     
-    def page_move(request, idid):
+    def page_move(request, idid):# ページ遷移用
         id_page = get_object_or_404(NoticeList, pk=idid)
         return render(request, 'contents/n_test.html', {'post': id_page})
 
     # """
-class Notice_(ListView):
+class Notice_(TemplateView):
     template_name = "contents/n_test.html"# template_name = 'contents/notice.html'
-    model = NoticeList
+    # model = NoticeList
 
     # def get_queryset(self):
     #     idid = self.kwargs['idid']
     #     nl2 = NoticeList.object.filter(id=idid)
-    #     NoticeList.object.all()
+        
         
     #     return nl2
-    
+    # nl = 
+    print("a")
     def get_queryset(self, **kwargs):
         queryset = super().get_queryset(**kwargs) # Article.objects.all() と同じ結果
 
@@ -53,9 +56,11 @@ class Notice_(ListView):
 
         # is_publishedがTrueのものに絞り、titleをキーに並び変える
         queryset = queryset.filter()
-
+        print("\n")
+        print(queryset)
+        print("b")
         return queryset
-    
+    print("c")
 
     
 class AccessError(TemplateView):
