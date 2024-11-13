cecb023c4ab7e1ca80a2a90df7c548d64a1128d0
yoshi <tky2302104@stu.o-hara.ac.jp>
2024-11-13 12:08:36 +0900

    post/urls.py修正

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,4 @@ app_name = 'post'
 
 urlpatterns = [
     path('post/', views.PostPageView.as_view(), name='post'),
-    path('post_complete/', views.UpdatePostInfo.as_view(), name='update_post'),
 ]
