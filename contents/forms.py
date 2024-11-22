715a814ece2fc5150df74669fd0af839268b602b
tky2302107 <149656519+tky2302107@users.noreply.github.com>
2024-11-22 12:35:00 +0900

    フォロー解除機能追加

--- contents/forms.py
+++ contents/forms.py
@@ -12,3 +12,15 @@ class ClickFollowForm(ModelForm):
             "follow_name",
         )
     # follow_name = forms.CharField()
+
+class FollowDisableForm(forms.Form):
+    click = forms.CharField(
+        label='forms',
+        max_length=100,
+        required=True,
+    )
+
+# class FollowDisable(forms.Input):
+#     class Meta:
+#             fie
+
