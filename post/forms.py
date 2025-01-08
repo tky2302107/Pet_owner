71f88f905a0a02a67a27555e4df5d731070128e6
yoshi <tky2302104@stu.o-hara.ac.jp>
2025-01-08 15:15:00 +0900

    投稿閲覧画面のレイアウトを他の画面と統一しました

--- post/forms.py
+++ post/forms.py
@@ -3,7 +3,7 @@ from .models import PostInfo
 
 # 投稿フォーム
 class PostForm(forms.ModelForm):
-    post = forms.CharField(widget=forms.Textarea) # 投稿文
+    post = forms.CharField(widget=forms.Textarea(attrs={"cols":100, "rows":10, "font-size": "100%"})) # 投稿文
     image = forms.ImageField(required=False) # 画像
     video = forms.FileField(required=False, widget=forms.FileInput(attrs={'accept':'video/*'})) # 動画
 
