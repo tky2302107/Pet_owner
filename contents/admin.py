829d640397f6c32d9c3fb0b25445c7ea3aca952e
tky2302107 <149656519+tky2302107@users.noreply.github.com>
2025-01-10 12:20:59 +0900

    管理画面設定(accounts,contents)

--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from contents.models import NoticeList,HospitalList
+admin.site.register(NoticeList)
+admin.site.register(HospitalList)
